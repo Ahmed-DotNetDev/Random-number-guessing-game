dbd3609aedcd55b0bd5eae1cd2e63b6a518f3a9e
Ahmed.net <Ahmed.net@gmail.com>
2023-03-02 14:39:43 +0200

    Adding random number game project

---
+++ Random-number-guessing-game/Random-number-guessing-game/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Random_number_guessing_game
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Random random = new Random();
+			int RandomNumber = random.Next(1, 10);
+			int GuessNumber = 0;
+			while (GuessNumber != RandomNumber)
+			{
+				Console.WriteLine("I am thinking of a number between 1 →→ 100.  Can you guess what it it: ? ");
+				
+				while (GuessNumber < RandomNumber)
+				{
+					GuessNumber = Convert.ToInt32(Console.ReadLine());
+					Console.WriteLine("Increase your value ++++");
+
+				}
+
+				while (GuessNumber > RandomNumber)
+				{
+					GuessNumber = Convert.ToInt32(Console.ReadLine());
+					Console.WriteLine("decrease your value ----");
+
+				}
+			}
+			while (GuessNumber == RandomNumber)
+			{
+				Console.WriteLine(" Done ✔ ");
+				break;
+			}
+		}
+	}
+}
